48d2e323dc1590c73e69a612a1e6e7aaaeba6172
kacper.krysiak <kacperkrysiak99@gmail.com>
2023-03-10 15:47:32 +0100

    Nowy upload

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
     return render(request, 'app/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_list_or_404(Post, pk=pk)
+    return render(request, 'app/post_detail.html', {'post': post})
